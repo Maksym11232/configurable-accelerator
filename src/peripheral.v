05e89e43d43992f4b4d883ddae2c8d033c7d7fda
Maksym <maksym.podgorski@gmail.com>
2025-07-24 22:50:56 +0200

    Update peripheral.v

--- src/peripheral.v
+++ src/peripheral.v
@@ -26,80 +26,13 @@ module accelerator (
     output [7:0]  data_out      // Data out from the peripheral, set this in accordance with the supplied address
 );
 
-    logic [7:0] reg_A;
-    logic [7:0] reg_B;
-    logic [7:0] reg_C;
-    logic [7:0] reg_D;
-
-    logic [3:0] op_1;
-    logic [1:0] input_a_1;
-    logic [1:0] input_b_1;
-    logic [1:0] output_1;
-    
-    
-    logic [3:0] alu_op;
-    logic [7:0] alu_out;
-    logic [1:0] alu_sel_a;
-    logic [1:0] alu_sel_b;
-
-    alu alu(
-        .A(reg_A),
-        .B(reg_B),
-        .C(reg_C),
-        .D(reg_D),
-
-        .sel_a(alu_sel_a),
-        .sel_b(alu_sel_b),
-        
-        .op(alu_op),
-        .out(alu_out)
-    );
-
     
     always @(posedge clk) begin
         if (!rst_n) begin
-            reg_A <= 0;
-            reg_B <= 0;
-            reg_C <= 0;
-            reg_D <= 0;
             
-            alu_op <= 0;
-            alu_out <= 0;
-            alu_sel_a <= 0;
-            alu_sel_b <= 0;
-            
-            op_1 <= 0;
-            input_a_1 <= 0;
-            input_b_1 <= 0;
-            output_1 <= 0;
         end else if (data_write) begin
-            case (address)
-                4'h0: reg_A <= data_in;
-                4'h1: reg_B <= data_in;
-                4'h2: reg_C <= data_in;
-                4'h3: reg_D <= data_in;
-                4'h4: op_1 <= data_in[3:0];
-                4'h7: begin
-                    input_a_1 <= data_in[1:0];
-                    input_b_1 <= data_in[3:2];
-                    output_1 <= data_in[5:4];
-                      end
-                default: ;
-            endcase
-            alu_op <= op_1;
-            alu_sel_a <= input_a_1;
-            alu_sel_b <= input_b_1;
-        end
+            
     end
-
-    assign data_out =
-        (address == 4'h0) ? reg_A :         // Read A
-        (address == 4'h1) ? reg_B :         // Read B
-        (address == 4'h2) ? reg_C :         // Read C
-        (address == 4'h3) ? reg_D :         // Read D
-        (address == 4'h4) ? {4'b0, op_1} : // Read Opcode
-        (address == 4'h5) ? alu_out : // Read Result
-        8'h00;
 
 
     
@@ -111,48 +44,4 @@ assign uo_out = 8'b0;             // Default output (or set only used bits)
 
     
 
-endmodule
-
-
-module alu(
-    input logic [7:0] A,
-    input logic [7:0] B,
-    input logic [7:0] C,
-    input logic [7:0] D,
-
-    input logic [1:0] sel_a,
-    input logic [1:0] sel_b,
-    
-    input logic [3:0] op,
-
-    output logic [7:0] out
-);
-
-    logic [7:0] alu_a;
-    logic [7:0] alu_b;
-    
-    always_comb begin
-        case (sel_a)
-            2'b00: alu_a = A;
-            2'b01: alu_a = B;
-            2'b10: alu_a = C;
-            2'b11: alu_a = D;
-        endcase
-
-        case (sel_b)
-            2'b00: alu_b = A;
-            2'b01: alu_b = B;
-            2'b10: alu_b = C;
-            2'b11: alu_b = D;
-        endcase
-                
-        case (op)
-            4'b0000: out = alu_a + alu_b;
-            4'b0001: out = alu_a - alu_b;
-            default: out = 8'b0;
-        endcase
-    end
-
-
-    
 endmodule
